3748a0d43cf55e9ab6f4715e2b5117d43294907d
zhengyscn <zhengyscn@gmail.com>
2018-08-28 01:16:13 +0800

    add file-tell

--- advanced/file/logstash.py
+++ advanced/file/logstash.py
@@ -3,9 +3,9 @@ import json
 import time
 
 '''小文件读取'''
-def readFile(FILENAME):
+def readFile(filename):
     try:
-        fd = open(FILENAME, 'r')
+        fd = open(filename, 'r')
         return fd.read(), True
     except Exception as e:
         return e.args, False
@@ -14,13 +14,13 @@ def readFile(FILENAME):
             fd.close()
 
 '''通用 写文件'''
-def writeFile(FILENAME, DATA):
+def writeFile(filename, data):
     try:
-        fd = open(FILENAME, 'w')
-        if isinstance(DATA, int):
-            return fd.write(str(DATA)), True
-        elif isinstance(DATA, list) or isinstance(DATA, dict):
-            return fd.write(json.dumps(DATA)), True
+        fd = open(filename, 'w')
+        if isinstance(data, int):
+            return fd.write(str(data)), True
+        elif isinstance(data, list) or isinstance(data, dict):
+            return fd.write(json.dumps(data)), True
         else:
             return "file isinstance(data) match failed.", False
     except Exception as e:
@@ -30,12 +30,12 @@ def writeFile(FILENAME, DATA):
             fd.close()
 
 '''获取文件指针'''
-def getFilePos(FILENAME):
-    if not os.path.exists(FILENAME):
-        os.makedirs(os.path.dirname(FILENAME))
+def getFilePos(filename):
+    if not os.path.exists(filename):
+        os.makedirs(os.path.dirname(filename))
         FILE_POS = 0
     else:
-        pid, ok = readFile(FILENAME)
+        pid, ok = readFile(filename)
         if ok:
             try:
                 FILE_POS = int(pid.strip())
@@ -47,17 +47,17 @@ def getFilePos(FILENAME):
     return FILE_POS
 
 '''流式处理'''
-def fileStream(LOG_FILE, PID_FILE, FILE_POS):
+def fileStream(log_file, pid_file, file_pos):
     try:
-        fd = open(LOG_FILE, 'r')
-        fd.seek(FILE_POS)
+        fd = open(log_file, 'r')
+        fd.seek(file_pos)
         for line in fd:
             print(line, end="")
-            FILE_POS += len(line)
+            file_pos += len(line)
             time.sleep(0.3)
     except KeyboardInterrupt:
         '''捕获CTRL + C'''
-        msg, ok = writeFile(PID_FILE, FILE_POS)
+        msg, ok = writeFile(pid_file, file_pos)
         if not ok:
             print(msg)
     finally:
@@ -70,6 +70,7 @@ def main():
     # print(os.path.abspath(__file__))
     # print(os.path.dirname(os.path.abspath(__file__)))
 
+    '''获取文件绝对路径'''
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     LOG_FILE = os.path.join(BASE_DIR, "mysqld.log")
     PID_FILE = os.path.join(BASE_DIR, 'run', 'logstash.pid')
